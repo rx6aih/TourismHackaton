7f66b98e68b4d49640468ed1fbbc5b13dea0a4c7
rx6aih <mrsaintsteam@mail.ru>
2025-04-13 11:35:58 +0300

    JsonPropertyName for RequestRecDto

--- Tourism.Dotnet.Parser/Utility/Dto/RequestRecDto.cs
+++ Tourism.Dotnet.Parser/Utility/Dto/RequestRecDto.cs
@@ -1,8 +1,11 @@
+using System.Text.Json.Serialization;
+
 namespace Tourism.Dotnet.Parser.Utility.Dto;
 
 public class RequestRecDto
 {
     public string? Query { get; set; } = string.Empty;
+    [JsonPropertyName("places")]
     public RecommendationDto[]? Recommendations { get; set; }
     public string Language { get; set; } = "english";
 }
